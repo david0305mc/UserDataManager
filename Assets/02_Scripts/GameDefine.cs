5198422cd7e68891b661e164890230316a2e0d40
david0305 <david0305mc@gmail.com>
2025-08-27 12:05:56 +0900

    Added TextMeshUI, Transition Scene

---
+++ Assets/02_Scripts/GameDefine.cs
@@ -0,0 +1,9 @@
+
+
+public static class GameDefine
+{
+    public const string IntroScene = "Intro";
+    public const string MainScene = "Main";
+
+
+}
--- Assets/02_Scripts/UIIntro.cs
+++ Assets/02_Scripts/UIIntro.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-// using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 
 public class UIIntro : MonoBehaviour
 {
@@ -11,19 +11,19 @@ public class UIIntro : MonoBehaviour
     void Awake()
     {
         Application.targetFrameRate = 120;
-        // _buttonStart.onClick.AddListener(() => StartGame().Forget());
+        _buttonStart.onClick.AddListener(() => StartGame().Forget());
     }
 
-    // private async UniTask StartGame()
-    // {
-    //     await DataManager.Instance.LoadDataAsync();
-    //     await DataManager.Instance.LoadConfigTable();
-    //     string uid = PlayerPrefs.GetString("uid", "1000");
-    //     await UserDataManager.Instance.LoadOrCreateAsync(uid);
-    //     PlayerPrefs.SetString("uid", uid);
+    private async UniTask StartGame()
+    {
+        // await DataManager.Instance.LoadDataAsync();
+        // await DataManager.Instance.LoadConfigTable();
+        string uid = PlayerPrefs.GetString("uid", "1000");
+        // await UserDataManager.Instance.LoadOrCreateAsync(uid);
+        PlayerPrefs.SetString("uid", uid);
         
-    //     var mainSceneAsync = SceneManager.LoadSceneAsync(GameDefine.MainScene);
-    //     await UniTask.WaitUntil(() => mainSceneAsync.isDone);
-    // }
+        var mainSceneAsync = SceneManager.LoadSceneAsync(GameDefine.MainScene);
+        await UniTask.WaitUntil(() => mainSceneAsync.isDone);
+    }
 
 }
